eb64796d2281121e51ea34598cb93fa2db451459
EinHelgi <einhelgi@gmail.com>
2014-11-05 12:13:41 +0000

    vona að þetta sé rétt

--- PacMan.js
+++ PacMan.js
@@ -1,18 +1,10 @@
-// =========
-// PAC-MAN
-// =========
-/*
-
-A sort-of-playable version of the classic arcade game.
-
-*/
+// ==========
+// PacMan STUFF
+// ==========
 
 "use strict";
 
 /* jshint browser: true, devel: true, globalstrict: true */
-
-var g_canvas = document.getElementById("myCanvas");
-var g_ctx = g_canvas.getContext("2d");
 
 /*
 0        1         2         3         4         5         6         7         8
@@ -20,167 +12,158 @@ var g_ctx = g_canvas.getContext("2d");
 */
 
 
-// ====================
-// CREATE PAC-MAN
-// ====================
+// A generic contructor which accepts an arbitrary descriptor object
+function PacMan(descr) {
 
-function createInitialPacMan() {
+    // Common inherited setup logic from Entity
+    this.setup(descr);
 
-    entityManager.generatePacMan({
-        cx : 224,
-        cy : 424
-    });
+    this.rememberResets();
     
-}
+    // Default sprite, if not otherwise specified
+    this.counter = 0;
+    this.animationOn = false;
+    this.i = 0;
+    this.sprite = this.sprite || g_animateSprites[this.i];
+   
+    // Set normal drawing scale, and warp state off
+    this._scale = 0.45;
+};
 
-// ======================
-// Fixing maze for tunnel
-// ======================
-g_maze.tiles[-1]=0;
-g_maze.tiles[-1][16]=0;
-g_maze.tiles[28]=0;
-g_maze.tiles[28][16]=0;
+PacMan.prototype = new Entity();
 
-// =============
-// GATHER INPUTS
-// =============
+PacMan.prototype.rememberResets = function () {
+    // Remember my reset positions
+    this.reset_cx = this.cx;
+    this.reset_cy = this.cy;
+    this.reset_rotation = this.rotation;
+};
 
-function gatherInputs() {
-    // Nothing to do here!
-    // The event handlers do everything we need for now.
-}
+PacMan.prototype.KEY_UP = 'W'.charCodeAt(0);
+PacMan.prototype.KEY_DOWN  = 'S'.charCodeAt(0);
+PacMan.prototype.KEY_LEFT   = 'A'.charCodeAt(0);
+PacMan.prototype.KEY_RIGHT  = 'D'.charCodeAt(0);
 
+PacMan.prototype.KEY_FIRE   = ' '.charCodeAt(0);
 
-// =================
-// UPDATE SIMULATION
-// =================
+// Initial, inheritable, default values
+PacMan.prototype.rotation = 0;
+PacMan.prototype.cx = 200;
+PacMan.prototype.cy = 200;
+PacMan.prototype.velX = 0;
+PacMan.prototype.velY = 0;
+PacMan.prototype.directionX = 1;
+PacMan.prototype.directionY = 1;
+PacMan.prototype.launchVel = 2;
+PacMan.prototype.numSubSteps = 1;
 
-// We take a very layered approach here...
-//
-// The primary `update` routine handles generic stuff such as
-// pausing, single-step, and time-handling.
-//
-// It then delegates the game-specific logic to `updateSimulation`
+// HACKED-IN AUDIO (no preloading)
+PacMan.prototype.warpSound = new Audio(
+    "sounds/shipWarp.ogg");
 
-
-// GAME-SPECIFIC UPDATE LOGIC
-
-function updateSimulation(du) {
-    
-    processDiagnostics();
-    
-    entityManager.update(du);
-
-    
-}
-
-// GAME-SPECIFIC DIAGNOSTICS
-
-var g_allowMixedActions = true;
-var g_useAveVel = true;
-var g_renderSpatialDebug = false;
-
-var KEY_MIXED   = keyCode('M');;
-var KEY_AVE_VEL = keyCode('V');
-var KEY_SPATIAL = keyCode('X');
-
-var KEY_HALT  = keyCode('H');
-var KEY_RESET = keyCode('R');
-
-var KEY_0 = keyCode('0');
-
-var KEY_1 = keyCode('1');
-var KEY_2 = keyCode('2');
-
-var KEY_K = keyCode('K');
-
-function processDiagnostics() {
-
-    if (eatKey(KEY_MIXED))
-        g_allowMixedActions = !g_allowMixedActions;
-
-    if (eatKey(KEY_AVE_VEL)) g_useAveVel = !g_useAveVel;
-
-    if (eatKey(KEY_SPATIAL)) g_renderSpatialDebug = !g_renderSpatialDebug;
-
-    
-}
-
-
-// =================
-// RENDER SIMULATION
-// =================
-
-// We take a very layered approach here...
-//
-// The primary `render` routine handles generic stuff such as
-// the diagnostic toggles (including screen-clearing).
-//
-// It then delegates the game-specific logic to `gameRender`
-
-
-// GAME-SPECIFIC RENDERING
-
-function renderSimulation(ctx) {
-
-    entityManager.render(ctx);
-
-    if (g_renderSpatialDebug) spatialManager.render(ctx);
-}
-
-
-// =============
-// PRELOAD STUFF
-// =============
-
-var g_images = {};
-
-function requestPreloads() {
-
-    var requiredImages = {
-        pacman_photo0: "images/pacman0.png",
-        pacman_photo1: "images/pacman1.png",
-        pacman_photo2: "images/pacman2.png",
-        pacman_photo3: "images/pacman3.png",
-        pacman_photo0left: "images/pacman0left.png",
-        pacman_photo1left: "images/pacman1left.png",
-        pacman_photo2left: "images/pacman2left.png",
-        pacman_photo3left: "images/pacman3left.png"
+PacMan.prototype.move = function(du) {
+    var rotation;
+    if(keys[this.KEY_UP]) {
+        if(this.animationOn === false) this.animationOn = true;
+        this.velX = 0;
+        this.velY = 3;
+        if(this.directionY > 0) {
+            this.directionY *= -1;
+        } 
+        this.rotation = -Math.PI/2;      
+    }
+    if(keys[this.KEY_DOWN]) {
+        if(this.animationOn === false) this.animationOn = true;
+        this.velX = 0;
+        this.velY = 3;
+        if(this.directionY < 0) {
+            this.directionY *= -1;
+        }       
+        this.rotation =  Math.PI/2;
+    }
+    if(keys[this.KEY_RIGHT]) {
+        if(this.animationOn === false) this.animationOn = true;
+        this.velX = 3;
+        this.velY = 0;
+        if(this.directionX < 0) {
+            this.directionX *= -1;
+        }       
+        this.rotation = 0;
+    }
+    if(keys[this.KEY_LEFT]) {
+        if(this.animationOn === false) this.animationOn = true;
+        this.velX = 3;
+        this.velY = 0;
+        if(this.directionX > 0) {
+            this.directionX *= -1;
+        }       
+        this.rotation = 0;
+    }
+    this.wrapPosition();
+    this.cx += this.velX*this.directionX*du;
+    this.cy += this.velY*this.directionY*du;
+    //change image
+    this.animate();
+};
+PacMan.prototype.animate = function(){
+    if(this.animationOn){
+        if(this.counter%5 === 0 && this.counter <= 10)this.i++;
+        if(this.counter%5 === 0 && this.counter > 10) this.i--;
+        if(this.directionX === -1){
+            this.sprite = g_animateSpritesLeft[this.i];
+        } else{
+            this.sprite = g_animateSprites[this.i];
+        }
+        //if(this.i === g_animateSprites.length-1) this.i=0;
+        this.counter++;
+        if(this.counter === 30) this.counter = 0;
+        }    
     };
 
-    imagesPreload(requiredImages, g_images, preloadDone);
-}
+PacMan.prototype.update = function (du) {
+    
+    spatialManager.unregister(this);
+    if(this._isDeadNow) return entityManager.KILL_ME_NOW;
 
-var g_sprites = {};
-var g_animateSprites = [];
-var g_animateSpritesLeft = [];
+ 
+    this.move(du);
 
-function preloadDone() {
+    // Handle firing
+   
 
-    var pacman0  = new Sprite(g_images.pacman_photo0),
-        pacman1  = new Sprite(g_images.pacman_photo1),
-        pacman2  = new Sprite(g_images.pacman_photo2),
-        pacman3  = new Sprite(g_images.pacman_photo3),
-        pacman0left = new Sprite(g_images.pacman_photo0left),
-        pacman1left = new Sprite(g_images.pacman_photo1left),
-        pacman2left = new Sprite(g_images.pacman_photo2left),
-        pacman3left = new Sprite(g_images.pacman_photo3left);
+    // TODO: YOUR STUFF HERE! --- Warp if isColliding, otherwise Register
+    if(this.isColliding()){
+        
+    } else {
+        spatialManager.register(this);
+    }
+};
 
-    g_animateSprites.push(pacman0);
-    g_animateSprites.push(pacman1);
-    g_animateSprites.push(pacman2);
-    g_animateSprites.push(pacman3);
+PacMan.prototype.getRadius = function () {
+    return (this.sprite.width / 2) * 0.9*this._scale;
+};
 
-    g_animateSpritesLeft.push(pacman0left);
-    g_animateSpritesLeft.push(pacman1left);
-    g_animateSpritesLeft.push(pacman2left);
-    g_animateSpritesLeft.push(pacman3left);
 
-    entityManager.init();
-    createInitialPacMan();
+PacMan.prototype.reset = function () {
+    this.setPos(this.reset_cx, this.reset_cy);
+    this.rotation = this.reset_rotation;
+    
+    this.halt();
+};
 
-    main.init();
-}
+PacMan.prototype.halt = function () {
+    this.velX = 0;
+    this.velY = 0;
+};
 
-// Kick it off
-requestPreloads();
+var NOMINAL_ROTATE_RATE = 0.1;
+
+PacMan.prototype.render = function (ctx) {
+    var origScale = this.sprite.scale;
+    // pass my scale into the sprite, for drawing
+    this.sprite.scale = this._scale;
+    this.sprite.drawWrappedCentredAt(
+    ctx, this.cx, this.cy, this.rotation
+    );
+    this.sprite.scale = origScale;
+};
