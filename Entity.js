227579725e07183cbd524b6fac5ba0cf906a6aba
EinHelgi <einhelgi@gmail.com>
2014-11-06 09:20:45 +0000

    farinn að undirbúa fyrir drauga

--- Entity.js
+++ Entity.js
@@ -64,19 +64,19 @@ Entity.prototype.centery = function (tileP) {
 };
 
 Entity.prototype.canGoLeft = function (tileP) {
-    return 0===g_maze.isThereWall(tileP[0]-1, tileP[1]);
+    return 1!==g_maze.isThereWall(tileP[0]-1, tileP[1]);
 }; 
 
 Entity.prototype.canGoRight = function (tileP) {
-    return 0===g_maze.isThereWall(tileP[0]+1, tileP[1]);
+    return 1!==g_maze.isThereWall(tileP[0]+1, tileP[1]);
 };
 
 Entity.prototype.canGoUp = function (tileP) {
-    return 0===g_maze.isThereWall(tileP[0], tileP[1]-1);
+    return 1!==g_maze.isThereWall(tileP[0], tileP[1]-1);
 };
 
 Entity.prototype.canGoDown = function (tileP) {
-    return 0===g_maze.isThereWall(tileP[0], tileP[1]+1);
+    return 1!==g_maze.isThereWall(tileP[0], tileP[1]+1);
 };
 
 Entity.prototype.tilePos = function () {
